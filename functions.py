c960483ee3d1facbb0a983ab5d76ecafc3f7f23f
alaayasser01 <77371903+alaayasser01@users.noreply.github.com>
2022-10-21 10:44:57 +0200

    delete and snr added

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 from telnetlib import X3PAD
+from tkinter import Y
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import plotly.graph_objects as go
@@ -9,6 +10,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+
 class Functions:
     #number of signals added
     numberSignalsAdded=-1
@@ -17,11 +19,10 @@ class Functions:
     ADDED_AMPLITUDES=[]
     ADDED_PHASES=[]
     ADDED_SIGNALS=[]
-
     amplitude_SUM = np.zeros(1000)  # Sum of AMPLITUDE of signals
     Current_amplitude=np.zeros(1000)
 
-x_Time = np.arange(0, 0.5, 0.0005).tolist()  # Time Axis Array
+x_Time = np.arange(0, 2, 0.0005).tolist()  # Time Axis Array
 
 def SHOW_SIN(magnitude, phase, frequency):  # Add new sin signal
 
@@ -30,7 +31,7 @@ def SHOW_SIN(magnitude, phase, frequency):  # Add new sin signal
         Y[i] = (magnitude * (math.sin((2 * np.pi * frequency * x_Time[i]) + phase)))
     return go.Figure([go.Scatter(x=x_Time, y=Y)])
 
-def SHOW_ADDED():  # Add new sin signal
+def SHOW_RES():  # Add new sin signal
     return go.Figure([go.Scatter(x=x_Time, y=Functions.Current_amplitude)])
 
 def ADD_SIGNAL(added_magnitude, added_phase,added_frequency):
@@ -48,15 +49,19 @@ def ADD_SIGNAL(added_magnitude, added_phase,added_frequency):
     print(Functions.ADDED_FREQUENCES)
     # print(Functions.ADDED_SIGNALS)
     print(Functions.numberSignalsAdded)
+    
 
     return go.Figure([go.Scatter(x=x_Time, y=Functions.Current_amplitude)])
 
-def DELETE_SIGNAL():  
-    Functions.Current_amplitude=Functions.ADDED_SIGNALS[Functions.numberSignalsAdded]  
-    Functions.ADDED_FREQUENCES.pop(Functions.numberSignalsAdded)
-    Functions.ADDED_AMPLITUDES.pop(Functions.numberSignalsAdded)
-    Functions.ADDED_PHASES.pop(Functions.numberSignalsAdded)
-    Functions.ADDED_SIGNALS.pop(Functions.numberSignalsAdded)
+def DELETE_SIGNAL(index_todelete): 
+    if(len(Functions.ADDED_SIGNALS)==0):
+        Functions.Current_amplitude=0
+    else:
+        Functions.Current_amplitude=np.subtract(Functions.Current_amplitude,Functions.ADDED_SIGNALS[index_todelete]  )
+    Functions.ADDED_FREQUENCES.pop(index_todelete)
+    Functions.ADDED_AMPLITUDES.pop(index_todelete)
+    Functions.ADDED_PHASES.pop(index_todelete)
+    Functions.ADDED_SIGNALS.pop(index_todelete)
 
     Functions.numberSignalsAdded-=1
     print(Functions.ADDED_FREQUENCES) 
@@ -64,14 +69,52 @@ def DELETE_SIGNAL():
     print(Functions.numberSignalsAdded)    
     return go.Figure([go.Scatter(x=x_Time, y=Functions.Current_amplitude)])
 
-def ADD_NOISE(signal, snrRatio):
-    power = signal**2
+def ADD_NOISE(snrRatio):
+    power = Functions.Current_amplitude**2
     snr_db = 10 * np.log10(snrRatio)
     signal_avg_power=np.mean(power)
     signal_avg_power_db=10 * np.log10(signal_avg_power)
     noise_db=signal_avg_power_db - snr_db
     noise_watts=10 ** (noise_db/10)
     mean_noise=0
-    noise=np.random.normal(mean_noise, np.sqrt(noise_watts),len(signal))
-    noise_signal = signal+noise
-    return noise_signal
+    noise=np.random.normal(mean_noise, np.sqrt(noise_watts),len(Functions.Current_amplitude))
+    Functions.Current_amplitude = Functions.Current_amplitude+noise
+    return go.Figure([go.Scatter(x=x_Time, y=Functions.Current_amplitude)])
+
+def SHOW_NOISE(snrRatio):
+    power = Functions.Current_amplitude**2
+    snr_db = 10 * np.log10(snrRatio)
+    signal_avg_power=np.mean(power)
+    signal_avg_power_db=10 * np.log10(signal_avg_power)
+    noise_db=signal_avg_power_db - snr_db
+    noise_watts=10 ** (noise_db/10)
+    mean_noise=0
+    noise=np.random.normal(mean_noise, np.sqrt(noise_watts),len(Functions.Current_amplitude))
+    noised_signal = Functions.Current_amplitude+noise
+    return go.Figure([go.Scatter(x=x_Time, y=noised_signal)])
+
+def ysampling(tsampling):
+    sampling_y=[]
+    for point in tsampling:
+        sampling_y.append(np.interp(point,x_Time, Functions.Current_amplitude))
+    return(sampling_y)
+        
+def sinc_interp(factor):
+    fmax= max(Functions.ADDED_FREQUENCES)
+    sampling_time= np.arange(0,2,1/(fmax*factor))
+    sampling_y= ysampling(sampling_time)
+    print(f"time={sampling_time}")
+    print(f"y={sampling_y}")
+
+    x_timeArr= np.array(x_Time)
+    # Find the period    
+    T = sampling_time[1] - sampling_time[0]
+    
+    sincM = np.tile(sampling_time, (len(x_timeArr), 1)) - np.tile(x_timeArr[:, np.newaxis], (1, len(sampling_time)))
+    newy = np.dot(sampling_y, np.sinc(sincM/T))
+    return go.Figure([go.Scatter(x=x_timeArr, y=newy)])
+
+    # sincM = np.tile(sampling_time, (len(x_timeArr), 1)) - np.tile(x_timeArr[:, np.newaxis], (1, len(sampling_time)))
+    # sampling_y= np.tile(sampling_y, (len(sampling_time), len(x_timeArr)))
+    # newy = np.dot(sampling_y, np.sinc(sincM/T))
+    # return go.Figure([go.Scatter(x=x_timeArr, y=newy)])
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import csv
 
 from pandas import *
 
+
 # general styling and tab name
 st.set_page_config(
     page_title="Sampling Studio",
@@ -23,47 +24,59 @@ st.text("Change Your analog Signal into digital & See How the Sampling frequancy
 #initiating df(dataframe) and empty fig
 TOADD_fig= px.density_heatmap(
          data_frame=[{}])
-Samp_fig= px.density_heatmap(
+samp_fig= px.density_heatmap(
          data_frame=[{}])
-ADDED_fig= px.density_heatmap(
+res_fig= px.density_heatmap(
          data_frame=[{}])
 
+res_fig=functions.SHOW_RES()
 #sidebar components
 with st.sidebar:
     # st.title('Generate, reconstruct and sample your signal')
-    tab0,tab1, tab2, tab3 = st.tabs(["Generate","Delete", "View" , "Sample"])
+    tab0,tab1, tab2, tab3 = st.tabs(["Generate","Delete", "SNR" , "Sample"])
     with tab0:
-        frq_value =st.number_input('signal frequancy', min_value= 0.0,value=1.0, step=1.0)
+        frq_value =st.number_input('signal frequancy', min_value= 0.0,value=0.0, step=1.0)
         amplitude_value =st.number_input('signal amplitude', min_value= 0.01,value=1.0, step=1.0)
         phase_value     =st.number_input('phase shift', min_value= 0,max_value=360,value=0, step=5)
-        snr_value           =st.slider('SNR ratio')        
-        # functions.ADD_NOISE(snr_value)
         if st.button('ADD signal'):
-            ADDED_fig=functions.ADD_SIGNAL(amplitude_value,phase_value,frq_value)
+            res_fig=functions.ADD_SIGNAL(amplitude_value,phase_value,frq_value)
     with tab1:
-        todelete_list=st.multiselect("choose the signal you want to delete",options=["added signals"],key='disabled' ,default=None)
-        if st.button(' DELETE signal'):
-            ADDED_fig=functions.DELETE_SIGNAL()
-    # with tab2:
-        # a list of added signals
-    with tab3: 
-        st.title('max freq =')
-        st.write('the sampling freq = sampling factor*max feq')
-        samp_factor= st.slider('sampling factor', 0.0 , 20.0 , 2.0, 0.5)
+        if(len(functions.Functions.ADDED_SIGNALS)):
+            todelete_list=[]
+            for signal in range(len(functions.Functions.ADDED_SIGNALS)):
+                todelete_list.append(st.checkbox(f"freq={functions.Functions.ADDED_FREQUENCES[signal]}, amp={functions.Functions.ADDED_AMPLITUDES[signal] }, phase={functions.Functions.ADDED_PHASES[signal]}",))
+            if st.button(' DELETE '):
+                for todeleteSigindex in range(len(todelete_list)):
+                    if(todelete_list[todeleteSigindex]):
+                        functions.DELETE_SIGNAL(todeleteSigindex)
+        else:
+            st.title("You have no added signals to delete")
+        # todelete_list=st.multiselect("choose the signal you want to delete",options=functions.Functions.ADDED_FREQUENCES,key='disabled' ,default=None)
+        # print(todelete_list)
+        # if st.button(' DELETE signal'):
+        #     for todelete_sig in todelete_list:
+        #         res_fig=functions.DELETE_SIGNAL(todelete_sig)
+    with tab2:
+        snr_value =st.slider('SNR ratio',0 , 10000,10000)
+        res_fig=functions.SHOW_NOISE(snr_value)     
+        if st.button('ADD noise'):
+            res_fig=functions.ADD_NOISE(snr_value)
+    # with tab3: 
+    #     if(len(functions.Functions.ADDED_FREQUENCES)>0):
+    #         maxFreq= max(functions.Functions.ADDED_FREQUENCES)
+    #         st.title(f'max freq= {maxFreq}') 
+    #         st.write('the sampling freq = sampling factor*max feq')
+    #         samp_factor= st.slider('sampling factor', 0.0 , 20.0 , 2.0, 0.5)
+    #         samp_fig=functions.sinc_interp(samp_factor)
+    #     else:
+    #         st.title("Construct your signal you want to sample first")
 
 
 TOADD_fig=functions.SHOW_SIN(amplitude_value,phase_value ,frq_value )
-Res_fig=functions.SHOW_ADDED()
 
-#add butthon
-# with st.form(key='form1'):
-#     freq_toadd=sampfrq_value 
-#     amplitude_toadd=amplitude_value 
-#     phase_toadd=phase_value 
-#     st.form_submit_button(label='ADD signal')
 
 #STREAMLIT COLUMNS AND ROWS 
-fig_uploaded,fig_sampled= st.columns((2))
+# fig_uploaded,fig_sampled= st.columns((2))
 empty1, fig_toadd ,emplty2, empty3 = st.columns((4))
 empty1, fig_res ,emplty2, empty3 = st.columns((4))
 empty1, fig_sampled ,emplty2, empty3 = st.columns((4))
@@ -74,9 +87,9 @@ with fig_toadd:
  
 with fig_res:
     st.markdown("### Resulted signal")
-    st.write(Res_fig)    
+    st.write(res_fig)    
  
 with fig_sampled:
     st.markdown("### Sampled signal")
-    st.write(Samp_fig)
+    st.write(samp_fig)
 
